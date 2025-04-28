eff283f4fe19e2efb089a1c52b3f7afaf65b763a
ACEITCTongzi <a0977050093@gmail.com>
2025-04-28 17:32:51 +0800

    Update CarSign.js

--- js/CarSign.js
+++ js/CarSign.js
@@ -11,23 +11,30 @@ function initMap() {
     });
 }
 
-// Firebase 配置
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js';
-import { getFirestore, collection, doc, setDoc, getDocs } from 'https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBv-DYm4c4l9Dn-o7ME4TnI92YsCpss1nM",
-  authDomain: "carsign-423fc.firebaseapp.com",
-  projectId: "carsign-423fc",
-  storageBucket: "carsign-423fc.appspot.com",
-  messagingSenderId: "219688439999",
-  appId: "1:219688439999:web:2d4f8646c98bcb76e4360a",
-  measurementId: "G-7FRW6JPXBJ"
-};
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// 初始化 Firebase 應用
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBv-DYm4c4l9Dn-o7ME4TnI92YsCpss1nM",
+    authDomain: "carsign-423fc.firebaseapp.com",
+    databaseURL: "https://carsign-423fc-default-rtdb.firebaseio.com",
+    projectId: "carsign-423fc",
+    storageBucket: "carsign-423fc.firebasestorage.app",
+    messagingSenderId: "219688439999",
+    appId: "1:219688439999:web:2d4f8646c98bcb76e4360a",
+    measurementId: "G-7FRW6JPXBJ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
 
 // 提交車輛位置
 function submitCarLocation() {
